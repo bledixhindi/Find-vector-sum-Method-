856c40dd48facc6b8577cc88c16795dc929000f1
bledixhindi <54402850+bledixhindi@users.noreply.github.com>
2023-01-17 19:55:35 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+	public static int vectorSum(int[] x) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < x.length; i++) {
+			
+			sum = sum + x[i];
+			
+		}
+		
+		return sum;
+	}
+	public static void main(String[] args) {
+		
+		int[] x = {1,2,3,4,5};
+		
+		System.out.println(vectorSum(x));
+	}
+
+
+}
